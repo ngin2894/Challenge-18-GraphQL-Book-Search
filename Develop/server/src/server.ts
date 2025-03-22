6960dbed0b2abbd6622c835d9231341078cb9892
ngin2894 <nataliegindraux@gmail.com>
2025-03-22 13:13:31 -0500

    wrote up initial server.ts file with Apollo server information per ReadMe requirements

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -2,20 +2,67 @@ import express from 'express';
 import path from 'node:path';
 import db from './config/connection.js';
 import routes from './routes/index.js';
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs, resolvers } from './graphql';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
+
+const startServer = async () => {
+  import express from 'express';
+  import path from 'node:path';
+  import { ApolloServer } from 'apollo-server-express';
+  import db from './config/connection.js';
+  import routes from './routes/index.js';
+  import { typeDefs, resolvers } from './graphql';
+
+  const app = express();
+  const PORT = process.env.PORT || 3001;
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  const startServer = async () => {
+    await server.start();
+    server.applyMiddleware({ app });
+
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+
+    if (process.env.NODE_ENV === 'production') {
+      app.use(express.static(path.join(__dirname, '../client/build')));
+    }
+
+    app.use(routes);
+
+    db.once('open', () => {
+      app.listen(PORT, () =>
+        console.log(`🌍 Now listening on localhost:${PORT}${server.graphqlPath}`)
+      );
+    });
+  };
+
+  startServer();
+  await server.start();
+  server.applyMiddleware({ app });
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.use(routes);
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
+};
